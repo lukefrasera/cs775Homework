9602f06b4d8ba4f6576e429e326f15f2d53e0904
Tim Sweet <timothy.l.sweet@gmail.com>
2016-02-21 21:29:43 -0800

    Add KNN classifier work

--- hw_001/scripts/least_squared_error_classify.py
+++ hw_001/scripts/least_squared_error_classify.py
@@ -1,18 +1,62 @@
 #!/usr/bin/env python
 import numpy as np
+from numpy import linalg as la
 import matplotlib.pyplot as plt
 import argparse
 import os
+import pdb
+from scipy import spatial
+import time
 '''
 Python Program demonstrating the use of a LSE (Least Squared Error) classifier.
 '''
 
+#KNNCLassifier returns a tuple of the K closest feature vectors
+def KNNSearch(k, features, test_data):
+    test_data_classification = []
+    for test_index, test_element in enumerate(test_data):
+        if test_element == []:
+            continue
+        neighborDistance = []
+        for feature_index,feature in enumerate(features):
+            try:
+                distance = la.norm(feature-test_element)
+            except ValueError:
+                pdb.set_trace()
+            neighborDistance.append([distance, feature_index])
+        neighborDistance = sorted(neighborDistance, key=lambda row: row[0], reverse=True)
+        #pdb.set_trace()
+        test_data_classification.append(np.matrix(neighborDistance[0:k][1]))
+    pdb.set_trace()
+    return test_data_classification
+    
+def KNNSearchFast(k, features, test_data):
+    t0 = time.time()
+    tree = spatial.KDTree(features)
+    t1 = time.time()
+    result = tree.query(test_data, k)
+    t2 = time.time()
+    print "Build time: %f, query time: %f" % (t1-t0, t2-t1)
+    return result
+    
+def KNNClassify(train_classification, test_neighbors):
+    test_classification = []
+    for sample in test_neighbors[1]:
+        votes = [0 for x in xrange(10)]
+        for neighbor in sample:
+            sample_class = train_classification(neighbor)
+            votes[sample_class] += 1
+        classification = max(enumerate(votes), key=operator.itemgetter(1))[0]
+        test_classification.append(classification)
+    return test_classification
+
 def ParseData(raw_data):
+    raw_data = raw_data.rstrip('\n')
     raw_data_list = raw_data.split('\n')
     data_list = list()
     for raw_data_point in raw_data_list:
+        raw_data_point = raw_data_point.rstrip()
         point = raw_data_point.split(' ')
-        point.pop()
         data_list.append([float(x) for x in point])
     data_list.pop()
     return np.array(data_list)
@@ -25,6 +69,7 @@ def main():
     parser.add_argument('-f', '--testing_file', type=str, help='submit data to test the trained model against')
     parser.add_argument('-s', '--save_model', type=str, help='save out trained model')
     parser.add_argument('-r', '--read_model', type=str, help='read in trained model')
+    parser.add_argument('-k', '--k_neighbors', type=int, help='number of neighbors to find')
     args = parser.parse_args()
 
     # Check if Arguments allow execution
@@ -47,11 +92,18 @@ def main():
             raw_data = file.read()
             # parse data
         data = ParseData(raw_data)
-
         # train on data
-        
-            
-
+        classification = data[:,0]
+        features = np.matrix(data[:,1:])
+    if args.testing_file:
+        with open(args.testing_file) as test_file:
+            raw_test_data = test_file.read()
+            test_data = ParseData(raw_test_data)
+            test_data_truth = test_data[:,0]
+            test_data = np.matrix(test_data[:,1:])
+        nearest_neighbors = KNNSearchFast(args.k_neighbors, features, test_data)
+        print nearest_neighbors
+        KNNClassify(classification, nearest_neighbors)
 
 if __name__ == '__main__':
     main()
