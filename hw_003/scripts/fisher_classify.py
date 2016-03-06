5351c5c7d4c7bb2b3a358e0fd74d52215e48daf9
Tim Sweet <timothy.l.sweet@gmail.com>
2016-03-05 19:09:23 -0800

    Fix the Gaussian build and computeGaussianProbability functions

--- hw_003/scripts/fisher_classify.py
+++ hw_003/scripts/fisher_classify.py
@@ -9,6 +9,7 @@ from scipy import spatial
 import time
 import operator
 
+
 def ParseData(raw_data, class1, class2):
     raw_data = raw_data.rstrip('\n')
     raw_data_list = raw_data.split('\n')
@@ -19,12 +20,57 @@ def ParseData(raw_data, class1, class2):
         data_list.append([float(x) for x in point])
     data_list.pop()
     data_list_np = np.array(data_list)
-    mask = (data_list_np[:,0] == class1) + (data_list_np[:,0] == class2)
+    mask = (data_list_np[:, 0] == class1) + (data_list_np[:, 0] == class2)
     data_list_np = data_list_np[mask]
     return data_list_np
 
-def main():
 
+def GaussianBuild(features, classification, class_id):
+    """
+    computes the mean and covariance for a dataset
+    :param features: s x f np.matrix (s samples by f features)
+    :param classification: s x 1 np.ndarray
+    :param class_id: scalar value to find
+    :return: [covariance(f x f),mean (f x 1)]
+    """
+    pdb.set_trace()
+    features_mask = (classification == class_id)
+    class_features = np.array(features)[features_mask].T
+    print 'Of ', features.shape, 'Elements, ', class_features.shape, ' are of class', class_id
+    cov_mat = np.cov(class_features)
+    mean_mat = np.mean(class_features, 1)
+    return [cov_mat, mean_mat]
+
+
+def ComputeGaussianProbability(cov_mat, mean_mat, sample):
+    """
+    computes the probability of a particular sample belonging to a particular gaussian distribution
+    :param cov_mat: f x f np.matrix (f features)
+    :param mean_mat: f x 1 np.matrix
+    :param sample: f x 1 np.matrix
+    :return:
+    """
+    mean_mat = np.matrix(mean_mat).T
+    sample = sample.T
+    # sample = meanMat
+    non_invertible = True
+    eye_scale = 0.0
+    cov_mat_inverse = 0
+    while non_invertible:
+        non_invertible = False
+        try:
+            cov_mat_inverse = la.inv(cov_mat + np.eye(cov_mat.shape[0]) * eye_scale)
+        except la.linalg.LinAlgError:
+            nonInvertible = True
+        eye_scale += 0.000000001
+    if eye_scale > 0.002:
+        print 'Set lambda to ', eye_scale, ' to make covMat invertible'
+    probability = 1.0 / (np.sqrt(la.norm(2 * np.pi * cov_mat)))
+    probability *= np.exp(-0.5 * (sample - mean_mat).T * cov_mat_inverse * (sample - mean_mat))
+    return probability
+
+
+def main():
     parser = argparse.ArgumentParser(description='Process input')
     parser.add_argument('-t', '--training_file', type=str, help='submit data to train against')
     parser.add_argument('-f', '--testing_file', type=str, help='submit data to test the trained model against')
@@ -57,17 +103,17 @@ def main():
             raw_data = file.read()
             # parse data
         data = ParseData(raw_data, args.classa, args.classb)
-        #plt.imshow(data[0,1:].reshape(1,256), cmap = plt.cm.Greys, interpolation = 'nearest')
-        #plt.show()
+        # plt.imshow(data[0,1:].reshape(1,256), cmap = plt.cm.Greys, interpolation = 'nearest')
+        # plt.show()
         # train on data
-        classification = data[:,0]
-        features = np.matrix(data[:,1:])
+        classification = data[:, 0]
+        features = np.matrix(data[:, 1:])
     if args.testing_file:
         with open(args.testing_file) as test_file:
             raw_test_data = test_file.read()
             test_data = ParseData(raw_test_data, args.classa, args.classb)
-            test_data_truth = test_data[:,0]
-            test_data = np.matrix(test_data[:,1:])
+            test_data_truth = test_data[:, 0]
+            test_data = np.matrix(test_data[:, 1:])
 
     if args.method == 0:
         print "good to go"
