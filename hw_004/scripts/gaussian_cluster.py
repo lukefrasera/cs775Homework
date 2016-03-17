1d355aae48ed3f66ae71535911d6b33de3e74d70
Tim Sweet <timothy.l.sweet@gmail.com>
2016-03-16 18:03:03 -0700

    Add outline of gaussian cluster

---
+++ hw_004/scripts/gaussian_cluster.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+import numpy as np
+from numpy import linalg as la
+import matplotlib.pyplot as plt
+import argparse
+import os
+import pdb
+from scipy import spatial
+import time
+import operator
+
+
+def ParseData(raw_data, class1, class2):
+    raw_data = raw_data.rstrip('\n')
+    raw_data_list = raw_data.split('\n')
+    data_list = list()
+    for raw_data_point in raw_data_list:
+        raw_data_point = raw_data_point.rstrip()
+        point = raw_data_point.split(' ')
+        data_list.append([float(x) for x in point])
+    data_list.pop()
+    data_list_np = np.array(data_list)
+    mask = (data_list_np[:, 0] == class1) + (data_list_np[:, 0] == class2)
+    data_list_np = data_list_np[mask]
+    return data_list_np
+
+def ComputeMahalanobisDistance(sample, mean, covariance):
+    return np.sqrt(
+
+def GaussianCluster(train_data, test_data, iterations, num_clusters):
+    '''
+    :param features:
+    :param classification:
+    :param test_data:
+    :return:
+    '''
+    #general algorithm: use the elements in features to construct the class separations
+    #then test the separations on the test data
+
+    #first, select random means for each cluster
+    num_features = train_data.shape(1)
+    num_train_samples = train_data.shape(2)
+    means = np.zeros((num_clusters,num_features))
+    covariance = np.zeros((num_train_samples, num_features, num_features));
+    for cluster in range(num_clusters):
+        sample_index = random.random() * num_train_samples
+        means(cluster) = train_data(sample_index)
+        covariance(cluster) = np.eye(num_features)
+    for iteration in range(iterations):
+        #assign all the data to a class
+        for sample in train_data:
+		distance = 
+            
+
+def GaussianBuild(features):
+    """
+    computes the mean and covariance for a dataset
+    :param features: s x f np.matrix (s samples by f features)
+    :param classification: s x 1 np.ndarray
+    :param class_id: scalar value to find
+    :return: [covariance(f x f),mean (f x 1)]
+    """
+    #pdb.set_trace()
+    print 'Of ', features.shape, 'Elements, ', features.shape
+    cov_mat = np.cov(features.T)
+    mean_mat = np.mean(features.T)
+    return [cov_mat, mean_mat]
+
+
+def ComputeGaussianProbability(cov_mat, mean_mat, sample):
+    """
+    computes the probability of a particular sample belonging to a particular gaussian distribution
+    :param cov_mat: f x f np.matrix (f features)
+    :param mean_mat: f x 1 np.matrix
+    :param sample: f x 1 np.matrix
+    :return:
+    """
+    mean_mat = np.matrix(mean_mat).T
+    sample = sample.T
+    # sample = meanMat
+    non_invertible = True
+    eye_scale = 0.0
+    cov_mat_inverse = 1.0 / cov_mat
+    probability = 1.0 / (np.sqrt(la.norm(2 * np.pi * cov_mat)))
+    probability *= np.exp(-0.5 * (sample - mean_mat).T * cov_mat_inverse * (sample - mean_mat))
+    return probability
+
+def main():
+    parser = argparse.ArgumentParser(description='Process input')
+    parser.add_argument('-t', '--training_file', type=str, help='submit data to train against')
+    parser.add_argument('-f', '--testing_file', type=str, help='submit data to test the trained model against')
+    parser.add_argument('-s', '--save_model', type=str, help='save out trained model')
+    parser.add_argument('-r', '--read_model', type=str, help='read in trained model')
+    parser.add_argument('-k', '--k_neighbors', type=int, help='number of neighbors to find')
+    parser.add_argument('-a', '--classa', type=int, help='class to test/train on')
+    parser.add_argument('-b', '--classb', type=int, help='class to test/train on')
+    parser.add_argument('-m', '--method', type=int, help='0=GaussCluster')
+
+    args = parser.parse_args()
+    print os.getcwd()
+    # Check if Arguments allow execution
+    if (not args.training_file) and (not args.read_model):
+        print "Error: No training Data or model present!"
+        return -1
+
+    if args.training_file and args.read_model:
+        print "Error: cannot read model and traing data at the same time!"
+        return -1
+
+    if args.training_file:
+        # trainagainst training file
+        if not os.path.isfile(args.training_file):
+            print "Error: Training file doesn't exist!"
+            return -1
+        # train
+        with open(args.training_file) as file:
+            # read file contents
+            raw_data = file.read()
+            # parse data
+        data = ParseData(raw_data, args.classa, args.classb)
+        # plt.imshow(data[0,1:].reshape(1,256), cmap = plt.cm.Greys, interpolation = 'nearest')
+        # plt.show()
+        # train on data
+        classification = data[:, 0]
+        features = np.matrix(data[:, 1:])
+    if args.testing_file:
+        with open(args.testing_file) as test_file:
+            raw_test_data = test_file.read()
+            test_data = ParseData(raw_test_data, args.classa, args.classb)
+            test_data_truth = test_data[:, 0]
+            test_data = np.matrix(test_data[:, 1:])
+
+    if args.method == 0:
+        result = FisherClassifier(features, classification, test_data, args.classa, args.classb)
+        print result
+        print [int(x) for x in list(test_data_truth)]
+        errors = np.array(result) == test_data_truth
+        class_a_samples = errors[test_data_truth == args.classa]
+        class_b_samples = errors[test_data_truth == args.classb]
+        num_a_correct = np.sum(class_a_samples)
+        num_b_correct = np.sum(class_b_samples)
+        total_a = class_a_samples.shape[0]
+        total_b = class_b_samples.shape[0]
+        print (1.0-float(num_a_correct)/total_a)*100,'%% of class a was misclassified'
+        print (1.0-float(num_b_correct)/total_b)*100,'%% of class b was misclassified'
+
+
+
+if __name__ == '__main__':
+    main()
+
