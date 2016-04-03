d3a8957d8aa5ffeaf4e8c1fa4135109dde0786a7
Luke Fraser <lukefrasera@gmail.com>
2016-04-03 00:58:08 -0700

    adding functionality to the neural net code. This includes adding a fast sigmoid function that can take advantage of multiple CPU's. The Sigmoid functionality is added as well the beginnings of the feed forward and backprop code. These two functions will be used to build an iteration of the complete neural network. Then it will become simple to perform the iterations and new options can be added to changes the way it performs

--- hw_005/scripts/neural_network.py
+++ hw_005/scripts/neural_network.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 import csv, sys
+from sympy import *
+import numpy as np
+import numexpr as ne
+
 
 class CSVInput:
     def __init__(self, filename, first_row_titles=False, num_convert=True, set_true_false_01=True):
@@ -31,10 +35,47 @@ class CSVInput:
                     self.data.append(row_list)
 
 
+class Sigmoid:
+    def __init__(self):
+        t = symbols('t')
+
+        self.sigmoid = 1 / (1 + exp(-t))
+        self.sigmoid_dif = self.sigmoid * (1 - self.sigmoid)
+        # print self.sigmoid
+        # print self.sigmoid_dif
+    def Sigmoid(self, t):
+        return ne.evaluate(str(self.sigmoid))
+    
+    def SigmoidPrime(self, t):
+        return ne.evaluate(str(self.sigmoid_dif))
+
+class NeuralNetwork:
+    def __init__(self, feature_size, compute_components, output_size):
+        self.o_zero = np.zeros(feature_size, 1)
+        self.W_one = np.random(feature_size, compute_components)
+        self.o_one = np.zeros(compute_components, 1)
+        self.W_two = np.random(compute_components, output_size)
+        self.S = Sigmoid()
+
+    def FeedForward(self, feature, truth):
+      # Compute the Feed forward pass of the first iteration on the Neural Network
+      # Need to append one onto the end of each of the weight and feature vector
+      self.o_zero = np.matrix(feature)
+      self.o_one = self.S(self.o_zero * self.W_one)
+
+    def BackProp(self):
+      pass
+
+
+    
 def main():
     # Read Data in and convert numbers to numbers
     reader = CSVInput(sys.argv[1], first_row_titles=True)
     print reader.titles
+
+    sigmoid = Sigmoid()
+    print sigmoid.Sigmoid([4,5,6,7,8,9])
+    print sigmoid.SigmoidPrime(np.array([4,5,6,7,8,9]))
     # print reader.data
 
 if __name__ == '__main__':
