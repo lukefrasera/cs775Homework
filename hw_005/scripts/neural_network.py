ee34141b703ffd79ac0d274dc68be5c62e838917
Luke Fraser <lukefrasera@gmail.com>
2016-04-03 22:42:44 -0700

    add new features for Gamma update suppot. These will need to have expressions added using the numexpr method so that the update is quick. as well most of the backprop has been generated, but it also untested. I couldn't not remember what the O_hats were. I htink there would just be o with an added homogenous coordinate.

--- hw_005/scripts/neural_network.py
+++ hw_005/scripts/neural_network.py
@@ -44,12 +44,39 @@ class Sigmoid:
         t = symbols('t')
         self.sigmoid = 1 / (1 + exp(-t))
         self.sigmoid_dif = self.sigmoid * (1 - self.sigmoid)
+        self.sigmoid_reverse = t * (1 - t)
 
     def Sigmoid(self, t):
         return ne.evaluate(str(self.sigmoid))
-    
+
     def SigmoidPrime(self, t):
         return ne.evaluate(str(self.sigmoid_dif))
+
+    def SigmoidReverse(self, t):
+        return ne.evaluate(str(self.sigmoid_reverse))
+
+class Gamma:
+    def __init__(self, initial):
+        self.gamma =  initial
+    def Update(self, E):
+        pass
+
+class GammaSpeed(Gamma):
+    def __init__(self, initial, u, d):
+        self.gamma = initial
+        self.u = u
+        self.d = d
+    def Update(self, E):
+        pass
+
+class GammaRPROP(Gamma):
+    def __init__(self, initial, u, d):
+        self.gamma = initial
+        self.u = u
+        self.d = d
+
+    def Update(self, E):
+        pass
 
 class NeuralNetwork:
     def __init__(self, feature_size, compute_components, output_size):
@@ -75,7 +102,7 @@ class NeuralNetwork:
       # pdb.set_trace()
       self.o_zero_bar[0, :-1] = np.matrix(feature)
       self.o_one = self.S.Sigmoid(self.o_zero_bar * self.W_one_bar)
-      
+
       self.o_one_bar[0, :-1] = self.o_one
       self.o_two = self.S.Sigmoid(self.o_one_bar * self.W_two_bar)
 
@@ -83,10 +110,25 @@ class NeuralNetwork:
       print self.error
 
     def BackProp(self):
-      pass
+        self.D_two = np.matrix(np.diag(self.S.SigmoidReverse(self.o_two_bar[0, :-1])))
+        self.D_one = np.matrix(np.diag(self.S.SigmoidReverse(self.o_one_bar[0, :-1])))
+
+        self.S_two = self.D_two * self.error
+        self.S_one = self.D_one * self.W_two * self.S_two
+
+        self.nabla_W_two_trans = - self.gamma * self.S_two * self.o_one  # I can't remember what the o_one_hats are from lexture so these expressions are missing something
+        self.nabla_W_one_trans = - self.gamma * self.S_one * self.o_zero # The same applies to this sepxression as well
+
+        # Now A decision based on the parameters of the neural network need to be made. In this case the Either we perform the offline approach(batch mode) or the online update)
 
     def UpdateWeights(self):
       pass
+
+    def OnlineNeuralNetwork(self, iterations, features, truth):
+        truth_matrix = 0  # This will prdoduce the necessary vectors for the error calculation at the end of each feed forward step
+
+    def BatchNeuralNetwork(self, iterations, features, truth):
+        truth_matrix = 0
 
 
     
