a30f6b14ff91c9662ba50d71b810c2152836c7e6
Luke Fraser <lukefrasera@gmail.com>
2016-04-08 01:04:17 -0700

    adding midterm tamplate code. base classes in place and working towards the initial setup o fclasses

---
+++ midterm/scripts/classifier.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+
+
+class Classifier(object):
+  def __init__(self):
+    pass
+  def Train(self, samples):
+    pass
+  def Classify(self, sample):
+    pass
+
+
+class Fisher(Classifier):
+  def __init__(self):
+    pass
+  def Train(self, samples):
+    pass
+  def Classify(self, sample):
+    pass
+
+class Regression(Classifier):
+  def __init__(self):
+    pass
+  def Train(self, samples):
+    pass
+  def Classify(self, sample):
+    pass
+
+class Gaussian(Classifier):
+  def __init__(self):
+    pass
+  def Train(self, samples):
+    pass
+  def Classify(self, sample):
+    pass
+
+class ClassiferTest(object):
+  def __init__(self, classifier, training_set, testing_set):
+    self.classifier = classifier
+    self.training_set = training_set
+    self.testing_set = testing_set
+
+  def Training(self):
+    pass
+  def Testing(self):
+    pass
+  def Results(self):
+    pass
+
+def GraphResults(results):
+  pass
+
+def GenerateTable(results):
+  pass
+
+def main():
+  ''' Test the classes for performance and corrrecness'''
+  classifier = Regression()
+
+  testing = ClassifierTest(classifier, training_set, testing_set)
+
+  testing.Training()
+  testing.Testing()
+  results = testing.Results()
+
+  GraphResults(results)
+  GenerateTable(results)
+
+if __name__ == '__main__':
+  main()
