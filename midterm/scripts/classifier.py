82e45cedc991c496516e311278a8790745f99e8d
Luke Fraser <lukefrasera@gmail.com>
2016-04-08 20:39:28 -0700

    adding to the fisher classifier

--- midterm/scripts/classifier.py
+++ midterm/scripts/classifier.py
@@ -14,20 +14,50 @@ class Classifier(object):
     pass
   def ReformatData(self):
     pass
+    return (samples, truth)
 
 class Fisher(Classifier):
-  def __init__(self):
-    pass
+  def __init__(self, class_a, class_b):
+    self.projection = []
+    self.c_a = class_a
+    self.c_b = class_b
+
   def Train(self, samples, truth):
-    pass
+    # solve for projection
+    a_samples = samples[truth == self.c_a]
+    b_samples = samples[truth == self.c_b]
+
+    # ompute mean and covariance
+    a_mean = np.mean(a_samples, 0).T
+    b_mean = np.mean(b_samples, 0).T
+
+    a_cov  = np.cov(a_samples.T)
+    b_cov  = np.cov(b_samples.T)
+
+    # Compute fisher criteria projection to one dimension
+    self.projection = la.inv(a_cov + b_cov) * (a_mean - b_mean)
+    self.projection /= la.norm(sel.projection)
+
+    # solve for guassians
+
+    # roject all of the data
+    a_projected = self.projection * a_samples
+    b_projected = self.projection * b_samples
+
+    # genreate gaussian classifier
   def Classify(self, sample):
     pass
-  def ReformatData(self):
-    pass
+  def ReformatData(self, samples, truth):
+    ref_samples = np.ones((len(samples)+1, len(samples[0])))
+    ref_samples[:, 1:] = np.array(samples)
+
+    ref_truth = np.array(truth)
+    return (ref_samples, ref_truth)
 
 class Regression(Classifier):
   def __init__(self):
     self.a = []
+
   def Train(self, samples, truth):
     samples = np.matrix(samples)
     truth = np.matrix(truth)
@@ -36,6 +66,13 @@ class Regression(Classifier):
   def Classify(self, samples):
     samples = np.matrix(samples)
     return samples * self.a
+
+  def ReformatData(self, samples, truth):
+    ref_samples = np.ones((len(smaples)+1, len(samples[0])))
+    ref_samples[:, 1:] = np.array(samples)
+
+    ref_truth = np.array(truth).T * 2 - 1
+    return (ref_samples, ref_truth)
 
 class Gaussian(Classifier):
   def __init__(self):
